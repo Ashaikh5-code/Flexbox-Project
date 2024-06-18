3d1ca5f3de8383d95e5e17ba87aaf020f1b1bbd3
Ahmed Shaikh <ahmedshaikh@Ahmeds-MBP.lan>
2024-06-18 06:21:39 -0400

    Remove unnecessary files

--- Test.py
+++
@@ -1,6 +0,0 @@
-# Install with pip install firecrawl-py
-from firecrawl import FirecrawlApp
-
-app = FirecrawlApp(api_key='fc-bae3fab0078c4bde93eaa6fb9bc44571')
-response = app.crawl_url('https://www.legalbluebook.com/bluebook/v21/quick-style-guide')
-print(response)
